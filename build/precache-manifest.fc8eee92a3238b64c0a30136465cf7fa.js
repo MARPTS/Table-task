16b1ac99bebec70a1e53dd24e9c2195229f9d850
wb-lh434334 <wb-lh434334@alibaba-inc.com>
2019-03-09 17:41:25 +0800

    feat: build program demo

---
+++ build/precache-manifest.fc8eee92a3238b64c0a30136465cf7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa8d098f41326e0bdc33",
+    "url": "/static/js/main.fa8d098f.chunk.js"
+  },
+  {
+    "revision": "aa4bb2d26b09b934b017",
+    "url": "/static/js/1.aa4bb2d2.chunk.js"
+  },
+  {
+    "revision": "fa8d098f41326e0bdc33",
+    "url": "/static/css/main.57a278db.chunk.css"
+  },
+  {
+    "revision": "aa4bb2d26b09b934b017",
+    "url": "/static/css/1.71d70088.chunk.css"
+  },
+  {
+    "revision": "4e739821481f361a96f47243ff48d01c",
+    "url": "/index.html"
+  }
+];
